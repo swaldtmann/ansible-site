e332a7c4bd38e339b5d8d0e76f980593f09537d8
Stephan Waldtmann <stephan@waldtmann.de>
2018-10-16 19:50:54 +0200

    Fixes

--- scripts/erg_lxadv.sh
+++ scripts/erg_lxadv.sh
@@ -2,6 +2,4 @@
 
 #set -x
 
-#ansible-playbook -i inventories/dev/hosts.ini playbooks/site.yml --vault-id keep/vault-password-file --list-hosts
-
 ansible-playbook -i inventories/dev/hosts.ini playbooks/lx_advanced_site.yml --vault-id keep/vault-password-file "$@"

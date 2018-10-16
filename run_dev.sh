97f23144cc83d071b5fc49721c986487e616f1e4
Stephan Waldtmann <stephan@waldtmann.de>
2018-10-16 13:31:16 +0000

    First dev tests

---
+++ run_dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#set -x
+
+#ansible-playbook -i inventories/dev/hosts.ini playbooks/site.yml --vault-id keep/vault-password-file --list-hosts
+
+ansible-playbook -i inventories/dev/hosts.ini playbooks/site.yml --vault-id keep/vault-password-file "$@"

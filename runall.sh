53eadfbe3737241cf4510ecaad6591ff01d3fccf
Stephan Waldtmann <stephan@waldtmann.de>
2018-10-17 16:36:44 +0200

    Polishing

--- runall.sh
+++ runall.sh
@@ -11,3 +11,4 @@ ansible-playbook -i inventories/dev/hosts.ini playbooks/win_basic_site.yml --vau
 ansible-playbook -i inventories/dev/hosts.ini playbooks/win_advanced_site.yml --vault-id keep/vault-password-file "$@"
 
 ansible-playbook -i inventories/test/hosts.ini playbooks/site_role.yml --vault-id keep/vault-password-file "$@"
+
